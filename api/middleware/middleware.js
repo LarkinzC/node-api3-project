a9b5f33e6dac01c9419a7404957ae1e52c04c160
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-01 19:30:43 -0800

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,12 +1,29 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   const timestamp = new Date().toLocaleString()
   const method = req.method
   const url = req.originalUrl
     console.log(`[${timestamp}] ${method} to ${url}`)
+    next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if(!user) {
+      res.status(404).json({
+        message: 'user not found'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'server crashed'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
