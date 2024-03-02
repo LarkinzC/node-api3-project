bf2f5df53c48a669a352de42a87ce6c085cde729
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-01 20:01:50 -0800

    putting

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -7,28 +7,39 @@ const {
 = require('../middleware/middleware')
 // The middleware functions also need to be required
 
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get('/', (req, res, next) => {
+  User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user)
+ res.json(req.user)
 });
 
-router.post('/', validateUser, (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-  console.log(req.name)
+    User.insert({ name: req.name })
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update({ id: req.params.id, name: req.body.name })
+  .then(updatedUser => {
+    res.status(201).json(updatedUser)
+  })
+  .catch(next)
   console.log(req.name)
 });
 
@@ -49,6 +60,14 @@ router.post('/:id/posts', validatePost, validateUserId, (req, res) => {
   // and another middleware to check that the request body is valid
   console.log(req.user, req.text)
 });
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: 'Not exactly what im looking for',
+    message: err.message,
+    stack: err.stack
+  })
+}) 
 
 // do not forget to export the router
 
